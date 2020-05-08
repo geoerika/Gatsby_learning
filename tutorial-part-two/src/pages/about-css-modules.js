c69f94ee25ba83e517157f18f9d7e80d9dac8b0d
Erika Szabo <erika.szabo.geo@gmail.com>
2020-05-08 10:38:23 -0400

    included user comp

--- tutorial-part-two/src/pages/about-css-modules.js
+++ tutorial-part-two/src/pages/about-css-modules.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 import Container from '../components/container'
+import User from '../components/user'
+// import styles from './about-css-modules.module.css'
+
+// console.log(styles)
 
 export default function About() {
   return (
     <Container>
       <h1>About CSS Modules</h1>
       <p>CSS Modules are cool</p>
+      <User
+        username="Jane Doe"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+        excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <User
+        username="Bob Smith"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+        excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
     </Container>
   )
 }
+
