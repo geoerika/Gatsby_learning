2cce9cf68950af919d486a112cd5c8d8c62690b4
Erika Szabo <erika.szabo.geo@gmail.com>
2020-05-08 16:19:37 -0400

    created typography.js

---
+++ tutorial-part-four/src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from 'typography'
+import kirkhamTheme from 'typography-theme-kirkham'
+
+const typography =  new typography(kirkhamTheme)
+
+export default typography
+export const rythm = typography.rythm
