3e20dede22650acf2785d5cd37ff12f9b6f04009
Erika Szabo <erika.szabo.geo@gmail.com>
2020-05-08 15:22:28 -0400

    created global navigation

--- tutorial-part-three/src/components/layout.js
+++ tutorial-part-three/src/components/layout.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link } from 'gatsby'
+
+const LinkList=props => (
+  <li style={{ display: 'inline-block', marginRight: '1rem' }}>
+    <Link to={props.to}>{ props.children }</Link>
+  </li>
+)
 
 export default function Layout({ children }) {
   return (
     <div style={{ margin: '3rem auto', maxWidth: 650, padding: '0 1rem' }}>
+      <header style={{ marginBottom: '1.5rem '}}>
+        <Link to='/' style={{ textShadow: 'none', backgroundImage: 'none' }}>
+          <h3 style={{ display: 'inline' }}>MySweetSite</h3>
+        </Link>
+        <ul style={{ listStyle: 'none', float: 'right' }}>
+          <LinkList to='/'>Home</LinkList>
+          <LinkList to='/about/'>About</LinkList>
+          <LinkList to='/contact/'>Contact</LinkList>
+        </ul>
+      </header>
       { children }
     </div>
   )
