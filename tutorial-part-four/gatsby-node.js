4e27968f21fb3c532327bd9072eef55ed787aa8b
Erika Szabo <erika.szabo.geo@gmail.com>
2020-05-08 21:02:32 -0400

    added path

--- tutorial-part-four/gatsby-node.js
+++ tutorial-part-four/gatsby-node.js
@@ -1,4 +1,5 @@
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require('path')
 
 // create slugs for markdown files
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -15,6 +16,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 
 // create pages for markdown files
 exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
   // **Note:** The graphql function call returns a Promise
   // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
   const result = await graphql(`
@@ -30,5 +32,15 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `)
-  console.log(JSON.stringify(result, null, 4))
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/blog-post.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: node.fields.slug,
+      },
+    })
+  })
 }
