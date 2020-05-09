5ac389041a82b8a7b70954068fffb9130bf0854f
Erika Szabo <erika.szabo.geo@gmail.com>
2020-05-08 20:36:10 -0400

    created slugs for markdown files

---
+++ tutorial-part-four/gatsby-node.js
@@ -0,0 +1,14 @@
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+// create slugs for Markdown files
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug
+    })
+  }
+}
